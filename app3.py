9fcb410d60294ca21b2b05d39f5608398fb717f7
firstquarterlee <firstquarterlee@gmail.com>
2023-07-26 22:34:06 +0900

    🍎for 반복문

--- app3.py
+++ app3.py
@@ -1,2 +1,19 @@
-for i in range(0, 10):
-    print('BMW 있어요!!!!!')
+# for i in range(0, 10):
+#    print('BMW 있어요')
+
+중고차들 = ['K5', 'BMW', 'Tico']
+
+# print(중고차들[0])
+# print(중고차들[1])
+# print(중고차들[2])
+
+for i in 중고차들:
+    print(i)
+
+for i in 중고차들:
+    print(i * 3)
+
+
+# for 반복문
+# 1. 코드 단순 반복할 때
+# 2. List에서 자료 하나씩 뽑을 때
